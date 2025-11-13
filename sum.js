c5153f8307c25feced9d45531251001b0d1b1a7d
Trinh Huy Nam <huynam@192.168.1.62>
2025-11-13 20:59:21 +0700

    feature: remove unuse

--- sum.js
+++ sum.js
@@ -3,10 +3,10 @@ module.exports = {
     // Other Jest configurations...
     coverageThreshold: {
         global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
         },
     },
     // You might also want to configure which files to collect coverage from:
@@ -15,9 +15,7 @@ module.exports = {
         "!src/index.js",
     ],
     sum: (a, b) => {
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('Arguments must be numbers')
-        }
+       
         return a + b
     }
 }
