732790de845eb152899556a10f82bdaffcf20692
Trinh Huy Nam <huynam@192.168.1.62>
2025-11-13 20:58:17 +0700

    feat: add coverage

--- sum.js
+++ sum.js
@@ -1,3 +1,23 @@
 module.exports = {
-    sum: (a, b) => a + b
+    // jest.config.js
+    // Other Jest configurations...
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+    // You might also want to configure which files to collect coverage from:
+    collectCoverageFrom: [
+        "src/**/*.{js,jsx,ts,tsx}",
+        "!src/index.js",
+    ],
+    sum: (a, b) => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('Arguments must be numbers')
+        }
+        return a + b
+    }
 }
